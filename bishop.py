b634079ffb0428fdd1a890adf088adcf3b67ebf5
UnGerardo <gerardojdls@gmail.com>
2021-01-04 14:12:36 -0800

    Improved getMoves method for bishop

--- bishop.py
+++ bishop.py
@@ -14,37 +14,39 @@ class Bishop(chesspiece.ChessPiece):
         # reset
         self.possibleMoves = [0, 0, 0, 0]
 
-        # get top right moves
-        shift = 1
-        while (self.x+shift <= 8) and (self.y-shift >= 1) and (not piecesDict[self.x+shift][self.y-shift]):
-            if shift == 1:
-                self.possibleMoves[0] = ll.LinkedList((self.x+shift, self.y-shift))
-            self.possibleMoves[0].append((self.x + shift, self.y - shift))
-            shift += 1
+        rightShift = self.x+1
+        leftShift = self.x-1
+        upShift = self.y-1
+        bottomShift = self.y+1
+        while rightShift <= 8 or leftShift >= 1 or upShift >= 1 or bottomShift <= 8:
+            # get top right moves
+            if (rightShift <= 8) and (upShift >= 1) and (not piecesDict[rightShift][upShift]):
+                if not self.possibleMoves[0]:
+                    self.possibleMoves[0] = ll.LinkedList((rightShift, upShift))
+                self.possibleMoves[0].append((rightShift, upShift))
 
-        # get bottom right moves
-        shift = 1
-        while (self.x+shift <= 8) and (self.y+shift <= 8) and (not piecesDict[self.x+shift][self.y+shift]):
-            if shift == 1:
-                self.possibleMoves[1] = ll.LinkedList((self.x+shift, self.y+shift))
-            self.possibleMoves[1].append((self.x + shift, self.y + shift))
-            shift += 1
+            # get bottom right moves
+            if (rightShift <= 8) and (bottomShift <= 8) and (not piecesDict[rightShift][bottomShift]):
+                if not self.possibleMoves[1]:
+                    self.possibleMoves[1] = ll.LinkedList((rightShift, bottomShift))
+                self.possibleMoves[1].append((rightShift, bottomShift))
 
-        # get bottom left moves
-        shift = 1
-        while (self.x-shift >= 1) and (self.y+shift <= 8) and (not piecesDict[self.x-shift][self.y+shift]):
-            if shift == 1:
-                self.possibleMoves[2] = ll.LinkedList((self.x-shift, self.y+shift))
-            self.possibleMoves[2].append((self.x - shift, self.y + shift))
-            shift += 1
+            # get bottom left moves
+            if (leftShift >= 1) and (bottomShift <= 8) and (not piecesDict[leftShift][bottomShift]):
+                if not self.possibleMoves[2]:
+                    self.possibleMoves[2] = ll.LinkedList((leftShift, bottomShift))
+                self.possibleMoves[2].append((leftShift, bottomShift))
 
-        # get top left moves
-        shift = 1
-        while (self.x-shift >= 1) and (self.y-shift >= 1) and (not piecesDict[self.x-shift][self.y-shift]):
-            if shift == 1:
-                self.possibleMoves[3] = ll.LinkedList((self.x-shift, self.y-shift))
-            self.possibleMoves[3].append((self.x - shift, self.y - shift))
-            shift += 1
+            # get top left moves
+            if (leftShift >= 1) and (upShift >= 1) and (not piecesDict[leftShift][upShift]):
+                if not self.possibleMoves[3]:
+                    self.possibleMoves[3] = ll.LinkedList((leftShift, upShift))
+                self.possibleMoves[3].append((leftShift, upShift))
+
+            rightShift += 1
+            leftShift -= 1
+            upShift -= 1
+            bottomShift += 1
 
     # showMoves method for each piece bc need to determine if any straight line is blocked (see second condition in if)
     def showMoves(self, display):
